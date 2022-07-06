8f7b7829200eb1bf240f10e175bbb9816cc75ef6
Melvin175 <60463973+melvin175@users.noreply.github.com>
2022-07-06 22:28:39 +0530

    Update MoviesCollection.tsx

--- videolibrary/components/MoviesCollection.tsx
+++ videolibrary/components/MoviesCollection.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const MoviesCollection = () => {
-  return <div className="text-4xl text-center mt-16">Movies Collections</div>;
+  return (
+    <div className="text-4xl text-center mt-16">
+      Movies Collections and more
+    </div>
+  );
 };
 export default MoviesCollection;
